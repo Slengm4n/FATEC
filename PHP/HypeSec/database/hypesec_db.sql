12707d4d7d3a3450bf69246b9c9e7e377114b264
Slengm4n <99735480+Slengm4n@users.noreply.github.com>
2024-12-06 01:31:43 -0300

    sql

---
+++ PHP/HypeSec/database/hypesec_db.sql
@@ -0,0 +1,51 @@
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 06, 2024 at 04:31 AM
+-- Server version: 8.3.0
+-- PHP Version: 8.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hypesec_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `pd_name` varchar(255) NOT NULL,
+  `pd_description` varchar(255) NOT NULL,
+  `pd_code` int NOT NULL,
+  `pd_category` varchar(255) NOT NULL,
+  `pd_quantity` int NOT NULL,
+  `pd_valor` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `pd_name`, `pd_description`, `pd_code`, `pd_category`, `pd_quantity`, `pd_valor`) VALUES
+(5, 'Moletom', 'Roupa', 123123, 'Roupas de frio', 1231, '1231');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
