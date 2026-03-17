9b8766571b83e118a0c4dcba9afb27499f5b241b
João Slengman <99735480+Slengm4n@users.noreply.github.com>
2026-03-17 09:00:02 -0300

    Add DS aula_01 and aula_02 Java projects
    Add two new Java lesson projects under DS: aula_01 and aula_02(lista_exercicio). Each project includes VS Code settings (.vscode/settings.json), a README, source files (Aula.java, Fibonacci.java, App.java) and compiled class files in bin. App.java contains a set of exercise solutions and converters; Fibonacci.java provides a recursive example; Aula.java is a small demo. Adds project configuration to generate output in bin and reference lib/*.jar.

---
+++ DS/aula_01/src/Aula.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Aula {
+    public static void main(String[] args) {
+     
+        int age = 20;
+        String name = "João Slengman";
+        boolean IsMale = true;
+        String cpf = "532841239123";
+        String tel = "11998600253";
+        int[] nums = {123434};
+
+
+        System.out.printf("Meu nome é "+name, "tenho"+age,"anos","e meu telefone é"+tel);
+
+    }
+}
---
+++ DS/aula_02(lista_exercicio)/src/App.java
@@ -0,0 +1,140 @@
+import java.io.OutputStream;
+import java.lang.classfile.attribute.SourceDebugExtensionAttribute;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+    Scanner scr = new Scanner(System.in);
+        //LISTA DE EXERCICIOS 1
+
+        //1. Faça um programa que leia um número inteiro e o imprima.
+        
+        System.out.println("Digite um número inteiro: ");
+        int numInt = scr.nextInt();
+        System.out.println(numInt);
+
+        //2. Faça um programa que leia um número real e o imprima.
+        System.out.println("Digite um número real:");
+        double numDouble = scr.nextDouble();
+        System.out.println(numDouble);
+
+        //3. Peça ao usuário para digitar três valores inteiros e imprima a soma deles.
+        System.out.println("Digite três números inteiro e receba soma deles: ");
+        int num1 = scr.nextInt();
+        int num2 = scr.nextInt();
+        int num3 = scr.nextInt();
+
+        int soma = num1+num2+num3;
+
+        System.out.println("O resultado é: "+ soma);
+
+        //4. Leia um número real e imprima o resultado do quadrado desse número.
+        System.out.println("Digite um número real para saber o quadrado dele: ");
+        double numSquare = scr.nextDouble();
+        double result = numSquare * numSquare;
+        System.out.println(result);
+
+        //5. Leia um número real e imprima a quinta parte desse número.
+        System.out.println("Digite um número real para receber a quinta parte dele: ");
+        double numDivisionPart = scr.nextDouble();
+        double resultNumDivisonPart = numDivisionPart / 5;
+        System.out.println(resultNumDivisonPart);
+
+        /**6. Leia uma temperatura em graus Celsius e apresente-a convertida em graus
+        Fahrenheit.
+        Fórmula: F = C × (9.0/5.0) + 32.0**/
+        System.out.println("Digite uma temperatura em °C para converter em Fahrenheit: ");
+        double celsiusTemperature = scr.nextDouble();
+        double converterCelsiusTemperature = celsiusTemperature * (9.0/5.0) + 32.0;
+        System.out.println("A temperatura convertida é: "+ converterCelsiusTemperature);
+
+        /**Leia uma temperatura em graus Fahrenheit e apresente-a convertida em
+        graus Celsius.
+        Fórmula: C = 5.0 × (F − 32.0) / 9.0**/
+        System.out.println("Digite um temperatura em Fahrenheit para converter para °C: ");
+        double fahrenheitTemperature = scr.nextDouble();
+        double converterFahrenheitTemperature = 5.0 * (fahrenheitTemperature - 32.0)/9.0;
+        System.out.println("A temperatura convertida é: "+ converterFahrenheitTemperature);
+
+        /**8. Leia uma temperatura em graus Kelvin e apresente-a convertida em graus
+        Celsius.
+        Fórmula: C = K − 273.15**/
+        System.out.println("Digite uma temperatura em °K para conveter para °C: ");
+        double kelvinTemperature = scr.nextDouble();
+        double converterKelvinTemperature = kelvinTemperature - 273.15;
+        System.out.println("A temperatura convertida é: "+converterKelvinTemperature);
+
+        /**9. Leia uma temperatura em graus Celsius e apresente-a convertida em graus
+        Kelvin.
+        Fórmula: K = C + 273.15**/
+        System.out.println("Digite uma temperatura em °C para converter em °K: ");
+        double celsiusTemperature = scr.nextDouble();
+        double converterCelsiusTemperature = celsiusTemperature + 273.15;
+        System.out.println("A temperatura convertida é: "+converterCelsiusTemperature);
+
+        /**10. Leia uma velocidade em km/h e apresente-a convertida em m/s.
+        Fórmula: M = K / 3.6**/
+        System.out.println("Digite uma velocidade em KM/h para converter em M/s: ");
+        double kmhVelocity = scr.nextDouble();
+        double converterKhmVelocity = kmhVelocity / 3.6;
+        System.out.println("A velocidade convertida é: "+converterKhmVelocity);
+
+        /**11. Leia uma velocidade em m/s e apresente-a convertida em km/h.
+        Fórmula: K = M × 3.6**/
+        System.out.println("Digite uma velocidade em M/s para converter em KM/h: ");
+        double msVelocity = scr.nextDouble();
+        double converterMsVelocity = msVelocity * 3.6;
+        System.out.println("A velocidade convertida é: "+converterMsVelocity);
+
+        /**12. Leia uma distância em milhas e apresente-a convertida em quilômetros.
+        Fórmula: K = 1.61 × M **/
+        System.out.println("Digite uma distânica em Milhas para converter em KM: ");
+        double mileDistance = scr.nextDouble();
+        double converterMileDistance = 1.61 * mileDistance;
+        System.out.println("A distância convertida é: "+converterMileDistance);
+
+        /**13. Leia uma distância em quilômetros e apresente-a convertida em milhas.
+        Fórmula: M = K / 1.61**/
+        System.out.println("Digite uma distância em KM para converter em Milhas: ");
+        double kilometerDistance = scr.nextDouble();
+        double converterKilometerDistance = kilometerDistance / 1.61;
+        System.out.println("A distância convetida é: "+converterKilometerDistance);
+
+        /**14. Leia um ângulo em graus e apresente-o convertido em radianos.
+        Fórmula: R = G × π / 180 (π = 3.14)**/
+        System.out.println("Digite um ângulo em ° para conveter em radianos: ");
+        double angle = scr.nextDouble();
+        double converterAngle = angle * 3.14159265359 / 180;
+        System.out.println("O ângulo convetido em radianos é: "+converterAngle);
+
+        /**15. Leia um ângulo em radianos e apresente-o convertido em graus.
+        Fórmula: G = R × 180 / π (π = 3.14)**/
+        System.out.println("Digite um valor em radianos para conveter em ângulo:: ");
+        double radius = scr.nextDouble();
+        double conveterRadius = radius * 180 / 3.14159265359;
+        System.out.println("O valor radiano convertido em ângulo é: "+conveterRadius);
+
+        /**16. Leia um valor de comprimento em polegadas e apresente-o convertido em
+        centímetros.
+        Fórmula: C = P × 2.54**/
+        System.out.println("Digite um comprimento em pol para converter em cm: ");
+        double pol = scr.nextDouble();
+        double converterPol = pol * 2.54;
+        System.out.println("O valor em pol para cm é: "+converterPol);
+
+        /**17. Leia um valor de comprimento em centímetros e apresente-o convertido
+        em polegadas.
+        Fórmula: P = C / 2.54**/
+        System.out.println("Digite um valor em cm para converter em pol: ");
+        double cm = scr.nextDouble();
+        double conveterCm = cm / 2.54;
+        System.out.println("O valor em cm para pol é: "+conveterCm);
+
+        /**18. Leia um volume em metros cúbicos (m3) e converta para litros.
+        Fórmula: L = 1000 × M**/
+        System.out.println("Digite um valor em metros cúbicos (m3) para converter em litros: ");
+        double metersCubic = scr.nextDouble();
+        double converterMetersCubic = 1000 * metersCubic;
+        System.out.println("O valor em metros cúbicos (m3) para litros é: " +converterMetersCubic);
+        }
+}
